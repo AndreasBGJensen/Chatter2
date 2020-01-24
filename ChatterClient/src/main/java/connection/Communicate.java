175d79ba82d7106d55fa62c226ae766c33d28dc7
Simon Woldbye-Lyng <s180943@student.dtu.dk>
2020-01-24 13:34:02 +0100

    process of creating different class for communication.

--- ChatterClient/src/main/java/connection/Communicate.java
+++ ChatterClient/src/main/java/connection/Communicate.java
@@ -1,39 +1,25 @@
 package connection;
 
-import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.nio.Buffer;
 
-public class Connector {
+public class Communicate {
 
-    private static final int PORT = 2020;
-    private static final String IP_DEFAULT = "127.0.0.1";
-
-    private Socket socket;
-    private String ip = IP_DEFAULT;
+//    private Socket socket;
+//
+//    private PrintWriter out;
+//    private BufferedReader in;
+//
+//    public Communicate(Socket socket){
+//        this.socket = socket;
+//    }
+/*
 
     private PrintWriter out;
     private BufferedReader in;
-
-    public void setIP(String ip) {
-        this.ip = ip;
-    }
-
-    public void connect() throws IOException {
-        try {
-            System.out.println("Connecting to server");
-            socket = new Socket(ip, PORT);
-            System.out.println("Connected to server");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
 
     public void listen() {
 
@@ -47,6 +33,7 @@ public class Connector {
             e.printStackTrace();
         }
     }
+
 
     public void write() {
 
@@ -77,11 +64,7 @@ public class Connector {
         }
     }
 
-    public static void main(String[] args) throws IOException {
-        System.out.println("Starting program");
-        Connector connector = new Connector();
-        connector.connect();
-        connector.communicate();
-    }
-
+ */
 }
+
+
